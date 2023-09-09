2c2dd5531d27808b25d6fbce844e673d538e8a27
Jeff Castro <133550490+Jscastro197@users.noreply.github.com>
2023-09-09 18:27:36 -0400

    main

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ non_numeric_columns = data_csv.select_dtypes(exclude=[np.number]).columns.tolist
 # Convert non-numeric columns to numeric
 data_csv[non_numeric_columns] = data_csv[non_numeric_columns].apply(pd.to_numeric, errors='coerce')
 
-data_csv.iloc[:, :-1].values
-
 # Convert the DataFrame to a numpy array
 data = data_csv.to_numpy()
 
+# Exclude the last column
+data = data[:, :-1]  # This removes the last column
+
 # Calculate the multivariate mean vector
+print("1. Compute the multivariate mean vector:")
 mean_vector = np.mean(data, axis=0)
 print(mean_vector)
 
 print("2. Compute the sample covariance matrix as inner products between the columns of the centered data matrix")
 centered_data = data - mean_vector
-
-cov1 = np.dot(centered_data.T,centered_data)/centered_data.shape[0]
-
+cov1 = np.dot(centered_data.T, centered_data) / centered_data.shape[0]
 print(cov1)
